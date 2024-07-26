e2e33ea423b47c7d1934ff9cac4a6fa4193ddd97
Tan op <155438888+tan4200@users.noreply.github.com>
2024-07-26 11:37:25 +0530

    Update SpeedCruzeDatabase.js

--- SpeedCruzeDatabase.js
+++ SpeedCruzeDatabase.js
@@ -1,33 +1,26 @@
-const libPrefix = "SpeedCruzeDatabase"
+const libPrefix = "SpeedCruzeDatabase_";
 
-function saveData(name,value){
-  
-
-
-  HTTP.get( {
-    url: "https://heterotrophic-water.000webhostapp.com/save_sc.php?user_tg_id="+name+"&value="+value 
-  } )
-
-
-
+function saveData(name, value) {
+  HTTP.get({
+    url: "https://heterotrophic-water.000webhostapp.com/save_sc.php?user_tg_id=" + name + "&value=" + value
+  });
 }
 
-
-function getData(name){
-  HTTP.get( {
-    url: "https://heterotrophic-water.000webhostapp.com/get_sc.php?user_tg_id="+name,
- success:libPrefix + 'SaveData'
-  } )
+function getData(name) {
+  HTTP.get({
+    url: "https://heterotrophic-water.000webhostapp.com/get_sc.php?user_tg_id=" + name,
+    success: libPrefix + 'onDataReceived'
+  });
 }
 
-function onLoading(){
-  return JSON.parse(content).value
+function onDataReceived() {
+  var data = JSON.parse(content);
+  return data.value;
 }
-
 
 publish({
   saveData: saveData,
-  getData: getData     
-})
+  getData: getData
+});
 
-on(libPrefix + 'SaveData', onLoading );
+on(libPrefix + 'onDataReceived', onDataReceived);
